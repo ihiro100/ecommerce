750636617b98c77316b574a4477cf36bc838f001
ihimanshuawasthi@gmail.com <ihimanshuawasthi@gmail.com>
2018-06-29 16:09:08 +0530

    milestone 1

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
 // Database Name
 // const dbName = 'Ecommerce';
 
@@ -46,6 +47,17 @@ UserSchema.pre('save', function(callback){
 UserSchema.methods.comparePassword = function(password) {
   return bcrypt.compareSync(password,this.password);
 }
+
+UserSchema.methods.gravatar = function(size) {
+  // console.log(this);
+  if(!this.size) size = 200;
+  if(!this.email) return 'https://gravatar.com/avatar/?s=' + size + '&d=retro';
+  let md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  // console.log("==============>",md5)
+  return 'https://gravatar.com/avatar/'+ md5 +'?s=' + size + '&d=retro';
+}
+
+
 
 module.exports = mongoose.model('User',UserSchema);
 
--- routes/user.js
+++ routes/user.js
@@ -35,6 +35,8 @@ router.post('/signup',(req,res,next)=>{
   user.profile.name = req.body.name;
   user.password = req.body.password;
   user.email = req.body.email;
+  user.profile.picture = user.gravatar();
+  console.log(user);
 
   // console.log(req.body);
   User.findOne({email: req.body.email},function(err,existingUser){
@@ -62,5 +64,25 @@ router.get('/logout',(req,res,next)=>{
   req.logout();
   res.redirect('/');
 })
+
+router.get('/edit-profile',(req,res,next)=>{
+  res.render('accounts/edit-profile',{message: req.flash('success')})
+})
+
+router.post('/edit-profile',(req,res,next)=>{
+  User.findOne({_id: req.user._id},(err,user)=>{
+    if(err) return next(err);
+
+    if(req.body.name) user.profile.name = req.body.name;
+    if(req.body.address) user.address = req.body.address;
+
+    user.save((err)=>{
+      if (err) next(err);
+      req.flash('success','Successfully edited your profile')
+      return res.redirect('/edit-profile');
+    })
+  })
+})
+
 
 module.exports = router;
